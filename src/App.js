33baa35b327eed3e263165b5af08c99d84bfe977
KaiserSov <sara310105@gmail.com>
2022-02-23 20:47:07 -0500

    Añadí formulario de "registro"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import UserTable from './components/userTable';
+import React, {useState} from 'react';
+import UserTable from './components/UserTable';
+import AddUserForm from './components/AddUserForm';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+  ]
+
+  //state
+  const [users, setUsers] = useState(usersData)
+
+  //Add users
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+  }
+
   return (
     <div className='container'>
      <h1>CRUD App with hooks</h1>
      <div className='flex-row'>
        <div className='flex-large'>
          <h2>Add user</h2>
+         <AddUserForm user={users} />
        </div>
        <div className='flex-large'>
          <h2>View users</h2>
-         <UserTable />
+         <UserTable users={users} />
        </div>
      </div>
     </div>
